0333e5461541c977f2466c7035746ef1eaadeda4
Andrey Sozonov <andreysoz@yandex.ru>
2026-03-24 17:07:43 +0000

    1_drft

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,16 @@ int main() {
     std::println("Connecting to exchanges...");
 
 #warning "TODO: Параллельно подключаемся к обеим биржам"
+/*
+    Ваш код здесь: используйте connect_sender для подключения к обеим биржам и получения WebSocket stream'ов.
+     - Не забудьте обернуть ioc в std::reference_wrapper при передаче в sender
+     - Сохраните полученные WebSocket stream'ы для дальнейшего использования
+*/
     std::println("🔌 Closing connections...");
 #warning "TODO: Graceful shutdown: параллельно закрываем соединения через close_sender"
-
+/*
+    Ваш код здесь: используйте close_sender для закрытия обоих WebSocket соединений.
+*/
     pool.request_stop();
     std::println("🏁 All jobs completed");
     return 0;
